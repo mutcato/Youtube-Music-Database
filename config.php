c26a876bfc07cad061f6aa95630f6df7388250f3
mutcato <mtokat1@gmail.com>
2015-06-07 13:04:32 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 
 define('FACEBOOK_APP_ID', '125962467592875');
-define('FACEBOOK_SECRET', '2a043a774bf2fddb871be1c472c6d1e7');
+define('FACEBOOK_SECRET', 'SECRET');
 define('FB_BASE_URL', 'http://apps.facebook.com/olmasigereken/');
 define('APP_BASE_URL', 'http://www.ytmdb.com/');
 define('LASTFM_APP_ID', '0254a6c86df6730e94861e840076d500');
@@ -23,6 +23,6 @@ define('MYSQL_HOSTNAME', 'localhost');
 define('USERNAME_INSERT', 'cruisear'); // olmasige_insert userın sadece insert ve select izinleri var 
 define('USERNAME_UPDATE', 'cruisear'); // olmasige_update userın sadece insert, update ve select izinleri var 
 define('USERNAME_SELECT', 'cruisear_select'); // olmasige_select userın sadece select izni var 
-define('PASSWORD', 'l122cmx6HB');
+define('PASSWORD', 'PW');
 define('DATABASE', 'cruisear_muzik');
 ?>
